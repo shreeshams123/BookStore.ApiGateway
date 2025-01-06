fc402e0f7a49634122dd985a8eb5e8aba3d3da49
shreeshams123 <[shreesha2002ms@gmail.com]>
2025-01-06 11:07:17 +0530

    [Shreesha] Added CORS

--- BookStore.ApiGateway/Program.cs
+++ BookStore.ApiGateway/Program.cs
@@ -3,6 +3,16 @@ using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddJsonFile("ocelot.json",optional:false,reloadOnChange:true);
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder =>
+    {
+        builder.WithOrigins("http://localhost:4200") // Frontend URL
+               .AllowAnyMethod()
+               .AllowAnyHeader()
+               .AllowCredentials();
+    });
+});
 
 builder.Services.AddOcelot(builder.Configuration);
 
@@ -12,6 +22,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+app.UseCors("AllowAll");
 await app.UseOcelot();
 
 // Configure the HTTP request pipeline.
